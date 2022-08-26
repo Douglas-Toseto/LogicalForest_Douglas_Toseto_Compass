8a8d3c8ce3df5a2d12f79853faf57d4a56ee511f
Douglas-Toseto <90508145+Douglas-Toseto@users.noreply.github.com>
2022-08-26 18:04:15 -0300

    Ultimo ex JS + inicio Koans

--- Curso de JavaScript/aula.js
+++ Curso de JavaScript/aula.js
@@ -338,32 +338,49 @@ console.log(lista);
 function verificaEntrada(){
     let nome = document.getElementById('nome').value;
     let convite = document.getElementById('convite').value;
-    switch (convite){
-        case 'Jhonathan': //pessoa que convidou
+    
+    //estava tentando fazer as seleções com Switch/Case aninhados
+    /* switch (convite){
+        case 'Jonatan': //pessoa que convidou
             switch (nome){ //convidados dessa pessoa
                 case 'Luiz':
                 case 'Carlos':
-                    document.getElementById("resultado").innerHTML = "Você pode entrar!";
+                    document.getElementById('resultado').innerHTML = "Você pode entrar!";
                     break;
                 default:
-                    document.getElementById("resultado").innerHTML = "Você NÃO pode entrar!";
+                    document.getElementById('resultado').innerHTML = "Você NÃO pode entrar!";
             }
         case 'Douglas': //pessoa que convidou
             switch (nome){ //convidados dessa pessoa
                 case 'José':
-                    document.getElementById("resultado").innerHTML = "Você pode entrar!";
+                    document.getElementById('resultado').innerHTML = "Você pode entrar!";
                     break;
                 default:
-                    document.getElementById("resultado").innerHTML = "Você NÃO pode entrar!";
+                    document.getElementById('resultado').innerHTML = "Você NÃO pode entrar!";
             }
         case 'Eduardo': //pessoa que convidou
             switch (nome){ //convidados dessa pessoa
                 case 'Maria':
                 case 'Filomena':
-                    document.getElementById("resultado").innerHTML = "Você pode entrar!";
-                    break:
+                    document.getElementById('resultado').innerHTML = "Você pode entrar!";
+                    break;
                 default:
-                    document.getElementById("resultado").innerHTML = "Você NÃO pode entrar!";
+                    document.getElementById('resultado').innerHTML = "Você NÃO pode entrar!";
             }
+        default:
+            document.getElementById('resultado').innerHTML = "Você NÃO pode entrar!";
+    } */
+
+    let listaConvidades = {
+        Jonatan : ['Luiz', 'Carlos'],
+        Douglas : ['José'],
+        Eduardo : ['Maria', 'Filomena']
+    }
+
+    if (Object.keys(listaConvidades).includes(convite) && listaConvidades[convite].includes(nome)){
+        document.getElementById('resultado').innerHTML = "Você pode entrar!";
+    } else {
+        document.getElementById('resultado').innerHTML = "Você NÃO pode entrar!";
     }
 }
+
